7b928afb0123a2aa305f31d854591a96e0fd47d6
terukey <teru-h@tokyo-gas.co.jp>
2024-12-04 22:11:31 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,6 @@
-const date = new Date();
-const [month, day, year] = [
-  date.getMonth(),
-  date.getDate(),
-  date.getFullYear(),
-];
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth()+1;
+const date = today.getDate();
 
-console.log(date);
-
+console.log(year+"年"+month+"月"+date+"日");
