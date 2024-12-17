1d5e7d7faf9bd2e427905a5b012ac3e07ce39c7b
terukey <teru-h@tokyo-gas.co.jp>
2024-12-17 09:25:23 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,6 @@
-const btn = document.getElementById('btn');
+const btn=document.getElementById('btn')；
+const text=document.getElementById('text')；
 
-btn.addEventListener('click', () => {
-    console.log('ボタンをクリックしました');
-    btn.textContent = 'Clicked!';
-});
+btn.addEventListener('click',()=>{
+  　text.textContent="ボタンをクリックしました"；
+})；
